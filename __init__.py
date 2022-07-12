097bfcc77c6b90f2d7bbcba6327dc4567792f6bd
Jose Ignacio <j.i.dealvearcardenas@student.tudelft.nl>
2022-07-12 16:33:22 +0200

    Updated init and gitignore

--- __init__.py
+++ __init__.py
@@ -1,15 +1,38 @@
-
-
 # User input
 if __name__ == '__main__':
-    from Occupancy_grid.user_input import load_user_input
-    from Occupancy_grid.DroneFlight import DroneFlight
+    # Import self created libraries
+    from user_input import load_user_input
+    from DataGathering import DataGathering
+    from _init_paths import init_paths
+
+    # Import Python libraries
+    import sys
+    from icecream import ic
+    import json
+
+    # Setup the system paths
+    init_paths()
+
+    # Load the user input
     args = load_user_input()
 
-    # Fly drone
-    drone_flight = DroneFlight(args.altitude_m, args.altitude_range_m, args.cell_size_m,
-                               args.ue4_airsim_conversion_units, args.robot_radius, args.sensors_lst, args.cameras_info,
-                               args.sample_rate, min_flight_distance_m=args.min_flight_distance_m,
-                               saved_vertices_filename=args.saved_vertices_filename,
-                               update_saved_vertices=args.update_saved_vertices, plot2D=args.plot2D, plot3D=args.plot3D)
-    drone_flight.run(navigation_type=args.navigation_type, start_point=args.start, goal_point=args.goal)
+    # Debugging with icecream
+    gettrace = getattr(sys, 'gettrace', None)
+    if gettrace() and not args.PSO_tuning_switch:
+        ic.enable()
+    else:
+        ic.disable()
+
+    # Retrieving the start position of the drone
+    location_json_file = "C:\\Users\jialv\OneDrive\Documentos\AirSim\settings.json"
+    with open(location_json_file) as f:
+        data = json.load(f)
+    vehicle_name = list(data['Vehicles'].keys())[0]
+    coord = data['Vehicles'][vehicle_name]
+    start_coord = (coord['X'], coord['Y'], coord['Z'])
+
+    data_gathering = DataGathering(vehicle_name=vehicle_name, vehicle_start_position=start_coord)
+    if args.PSO_tuning_switch:
+        data_gathering.pso_pid_tuning()
+    else:
+        data_gathering.gather_data_consecutive_runs()
--- _init_paths.py
+++ _init_paths.py
@@ -2,18 +2,18 @@ import os.path as osp
 import sys
 
 
-def add_path(path):
-    if path not in sys.path:
-        sys.path.insert(0, path)
+def init_paths():
+    def add_path(path):
+        if path not in sys.path:
+            sys.path.insert(0, path)
 
+    this_dir = osp.dirname(__file__)
 
-this_dir = osp.dirname(__file__)
+    # Add libs to PYTHONPATH
+    # Add failure types libraries
+    lib_path_failures = osp.join(this_dir, 'FailureTypes')
+    add_path(lib_path_failures)
 
-# Add libs to PYTHONPATH
-# Add failure types libraries
-lib_path_failures = osp.join(this_dir, 'FailureTypes')
-add_path(lib_path_failures)
-
-# Add Navigation Algorithms libraries
-lib_path_nav = osp.join(this_dir, 'PythonRobotics')
-add_path(lib_path_nav)
+    # Add Navigation Algorithms libraries
+    lib_path_nav = osp.join(this_dir, 'PythonRobotics')
+    add_path(lib_path_nav)
